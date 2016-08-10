ce1bdde60499de71589fa8e9167a13c3d14d12c2
sankjoseph <sankjoseph@gmail.com>
2016-08-11 00:28:51 +0530

    error file update
    error

--- SaveLife/app/src/main/java/lemonlabs/in/savelife/OnPostExecuteListener.java
+++
@@ -1,9 +0,0 @@
-package lemonlabs.in.savelife;
-
-/**
- * Created by Santhosh.Joseph on 17-06-2016.
- */
-public interface OnPostExecuteListener {
-    public void onSuccess(ModelClassMapper model);
-    public void onFailure();
-}
